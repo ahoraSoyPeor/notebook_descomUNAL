cd24a605dd82b0525efdfc3bfb04d8cf763acdca
osdajigu <odjimenezg@unal.edu.co>
2021-03-20 12:17:23 -0500

    fix minor bug

--- Data structures/Rmq.cpp
+++ Data structures/Rmq.cpp
@@ -2,15 +2,15 @@
 /// Tested: https://tinyurl.com/y739tcsj
 struct rmq {
   vector<vector<int> > table;
-  rmq(vector<int> &v) : table(v.size() + 1, vector<int>(20)) {
-    int n = v.size()+1;
+  rmq(vector<int> &v) : table(v.size(), vector<int>(20)) {
+    int n = v.size();
     for (int i = 0; i < n; i++) table[i][0] = v[i];
     for (int j = 1; (1<<j) <= n; j++)
       for (int i = 0; i + (1<<j-1) < n; i++)
-        table[i][j] = max(table[i][j-1], table[i + (1<<j-1)][j-1]);
+        table[i][j] = min(table[i][j-1], table[i + (1<<j-1)][j-1]);
   }
   int query(int a, int b) {
     int j = 31 - __builtin_clz(b-a+1);
-    return max(table[a][j], table[b-(1<<j)+1][j]);
+    return min(table[a][j], table[b-(1<<j)+1][j]);
   }
 };
