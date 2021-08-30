6d92164ae35b949003636c8806ed94e60ced0ba3
Osman Jimenez <odjimenezg@unal.edu.co>
2021-08-29 20:41:43 -0500

    [Datastructures][RMQ] review cache friendly implementation

--- Data structures/Rmq.cpp
+++ Data structures/Rmq.cpp
@@ -1,16 +1,16 @@
 /// Complexity: O(|N|*log|N|)
 /// Tested: https://tinyurl.com/y739tcsj
 struct rmq {
-  vector<vector<int> > table;
-  rmq(vector<int> &v) : table(v.size(), vector<int>(20)) {
+  vector<vector<int>> table;
+  rmq(vector<int> &v) : table(20, vector<int>(v.size())) {
     int n = v.size();
-    for (int i = 0; i < n; i++) table[i][0] = v[i];
+    for (int i = 0; i < n; i++) table[0][i] = v[i];
     for (int j = 1; (1<<j) <= n; j++)
       for (int i = 0; i + (1<<j-1) < n; i++)
-        table[i][j] = min(table[i][j-1], table[i + (1<<j-1)][j-1]);
+        table[j][i] = min(table[j-1][i], table[j-1][i + (1<<j-1)]);
   }
   int query(int a, int b) {
     int j = 31 - __builtin_clz(b-a+1);
-    return min(table[a][j], table[b-(1<<j)+1][j]);
+    return min(table[j][a], table[j][b-(1<<j)+1]);
   }
 };
