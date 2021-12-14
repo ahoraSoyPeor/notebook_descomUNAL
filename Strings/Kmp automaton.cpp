8595f805e8ed61eb79ed07ad373802ab01ab0c8c
Jonathan Carrillo <joacarrilloco@unal.edu.co>
2021-12-13 22:33:15 -0500

    removed annoying spaces

--- Strings/Kmp automaton.cpp
+++ Strings/Kmp automaton.cpp
@@ -1,8 +1,8 @@
 /// Complexity: O(|N|*alphabet)
 /// Tested: not yet
 const int alpha = 256;
-vector< vector< int > > kmp_automaton(string &t) {
-  vector< vector< int > > aut( t.size() + 1, vector< int > ( alpha ) );
+vector<vector<int>> kmp_automaton(string &t) {
+  vector<vector<int>> aut( t.size() + 1, vector<int> ( alpha ) );
   aut[0][ t[0] ] = 1;
   for(int i = 1, j = 0; i <= t.size(); ++i) {
     aut[i] = aut[j];
