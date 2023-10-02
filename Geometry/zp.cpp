94b83737e05f4d54d9f4c785f86c5eda5b4e9cfd
joacarrilloco <30640717+joacarrilloco@users.noreply.github.com>
2023-10-02 18:11:44 -0500

    Added closest pair problem

--- Geometry/zp.cpp
+++ Geometry/zp.cpp
@@ -237,6 +237,51 @@ Circle min_circle( vector< pt > v ) {
   return ans;
 }
 
+pair < pt, pt >  closest ( vector< pt > v ) {
+  sort( v.begin(), v.end() );
+
+  pair< pt, pt > ans;
+  lf d2 = INF;
+
+  function< void( int, int ) > solve = [&]( int l, int r ) {
+    if( l == r ) return;
+      
+    int mid = ( l + r ) / 2;
+    lf x_mid = v[mid].x;
+    solve( l, mid );
+    solve( mid + 1, r );
+
+    vector< pt > aux;
+    int p1 = l, p2 = mid + 1;
+    while ( p1 <= mid && p2 <= r ) {
+      if( v[p1].y < v[p2].y ) aux.push_back( v[p1++] );
+      else aux.push_back( v[p2++] );
+    }
+    while( p1 <= mid ) aux.push_back( v[p1++] );
+    while( p2 <= r ) aux.push_back( v[p2++] );
+
+    vector < pt > nb;
+    for( int i = l; i <= r; ++ i ) {
+      v[i] = aux[i-l];
+      lf dx = ( x_mid - v[i].x );
+      if( dx * dx < d2 )
+        nb.push_back( v[i] );
+    }
+
+    const int m = (int) nb.size();
+    for( int i = 0; i < m; ++ i ) {
+      for( int k = i + 1; k < m; ++ k ) {
+        lf dy = ( nb[k].y - nb[i].y );
+        if( dy * dy > d2 ) break;
+        lf nd2 = dis2( nb[i], nb[k] );
+        if( nd2 < d2 ) d2 = nd2, ans = {nb[i], nb[k]};
+      }
+    }
+  };
+  solve( 0, v.size() -1 );
+  return ans;
+}
+
 lf areaOfIntersectionOfTwoCircles( lf r1, lf r2, lf d ) {
   if( d >= r1 + r2 )
     return 0.0L;
