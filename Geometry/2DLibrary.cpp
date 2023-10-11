14e094e7cd77672a018e8f075d9df51bca16e4f3
joacarrilloco <30640717+joacarrilloco@users.noreply.github.com>
2023-10-11 12:37:07 -0500

    Added in angle function

--- Geometry/2DLibrary.cpp
+++ Geometry/2DLibrary.cpp
@@ -54,6 +54,13 @@ lf cross( pt p, pt q ) { return p.x * q.y - q.x * p.y ; }
 
 lf orient( pt a, pt b, pt c ) { return cross( b - a, c - a ); };
 
+bool in_angle( pt a, pt b, pt c, pt x ) {
+  //assert( fabsl( orient( a, b, c ) ) > E0 );
+  if( orient( a, b, c ) < -E0 )
+    return orient( a, b, x ) >= E0 || orient( a, c, x ) <= E0;
+  return orient( a, b, x ) >= E0 && orient( a, c, x ) <= E0;
+}
+
 struct line {
   pt nv;
   lf c;
