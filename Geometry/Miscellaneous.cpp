eb245f3960cfd2f00e772ee8c533a9aa5de22bd9
joacarrilloco <30640717+joacarrilloco@users.noreply.github.com>
2023-10-15 16:55:36 -0500

    Update Miscellaneous.cpp

--- Geometry/Miscellaneous.cpp
+++ Geometry/Miscellaneous.cpp
@@ -1,4 +1,3 @@
- 
 lf part(pt a, pt b, T r) {
   lf l = abs(a-b);
   pt p = (b-a)/l;
@@ -19,12 +18,6 @@ lf inter_cp(circle c, polygon p) {
   }
   return abs(ans);
 }
-struct circle{
-    point center; double r;
-    bool contain(point &p) { return abs(center - p) < r + eps;}
-};
-T cross(point a, point b) { return a.x*b.y - a.y*b.x; }
-
 bool circumcircle_contains( triangle tr, pt D ) {//triange CCW
   pt A = tr.vert[0] - D, B = tr.vert[1] - D, C = tr.vert[2] - D;
 
@@ -38,7 +31,6 @@ bool circumcircle_contains( triangle tr, pt D ) {//triange CCW
 
   return det1 + det2 + det3 > E0;
 }
-
 lf areaOfIntersectionOfTwoCircles( lf r1, lf r2, lf d ) {
   if( d >= r1 + r2 )
     return 0.0L;
@@ -50,7 +42,6 @@ lf areaOfIntersectionOfTwoCircles( lf r1, lf r2, lf d ) {
   lf a2 = r2 * r2 * ( betha - sinl( betha ) * cosl( betha ) );
   return a1 + a2;
 };
-
 const lf eps = 1e-9;
 typedef double T;
 struct pt {
@@ -66,25 +57,15 @@ struct pt {
   bool operator > (const pt &o) const { return y > o.y || (y == o.y && x > o.x); }
 };
 int cmp (lf a, lf b) { return (a + eps < b ? -1 :(b + eps < a ? 1 : 0)); }
-/** Already in complex **/
+
 T norm(pt a) { return a.x*a.x + a.y*a.y; }
 lf abs(pt a) { return sqrt(norm(a)); }
 lf arg(pt a) { return atan2(a.y, a.x); }
-ostream& operator << (ostream& os, pt &p) {
-  return os << "("<< p.x << "," << p.y << ")";
-}
-/***/
-istream &operator >> (istream &in, pt &p) {
-    T x, y; in >> x >> y;
-    p = {x, y};
-    return in;
-}
+
 T dot(pt a, pt b) { return a.x*b.x + a.y*b.y; }
 T cross(pt a, pt b) { return a.x*b.y - a.y*b.x; }
 T orient(pt a, pt b, pt c) { return cross(b-a,c-a); }
-//pt rot(pt p, lf a) { return {p.x*cos(a) - p.y*sin(a), p.x*sin(a) + p.y*cos(a)}; }
-//pt rot(pt p, double a) { return p * polar(1.0, a); } /// for complex
-//pt rotate_to_b(pt a, pt b, lf ang) { return rot(a-b, ang)+b; }
+
 pt rot90ccw(pt p) { return {-p.y, p.x}; }
 pt rot90cw(pt p) { return {p.y, -p.x}; }
 pt translate(pt p, pt v) { return p+v; }
@@ -92,20 +73,6 @@ pt scale(pt p, double f, pt c) { return c + (p-c)*f; }
 bool are_perp(pt v, pt w) { return dot(v,w) == 0; }
 int sign(T x) { return (T(0) < x) - (x < T(0)); }
 pt unit(pt a) { return a/abs(a); }
-
-bool in_angle(pt a, pt b, pt c, pt x) {
-  assert(orient(a,b,c) != 0);
-  if (orient(a,b,c) < 0) swap(b,c);
-  return orient(a,b,x) >= 0 && orient(a,c,x) <= 0;
-}
-
-//lf angle(pt a, pt b) { return acos(max(-1.0, min(1.0, dot(a,b)/abs(a)/abs(b)))); }
-//lf angle(pt a, pt b) { return atan2(cross(a, b), dot(a, b)); }
-/// returns vector to transform points
-pt get_linear_transformation(pt p, pt q, pt r, pt fp, pt fq) {
-  pt pq = q-p, num{cross(pq, fq-fp), dot(pq, fq-fp)};
-  return fp + pt{cross(r-p, num), dot(r-p, num)} / norm(pq);
-}
 
 bool half(pt p) { /// true if is in (0, 180]
   assert(p.x != 0 || p.y != 0); /// the argument of (0,0) is undefined
@@ -117,36 +84,6 @@ bool half_from(pt p, pt v = {1, 0}) {
 bool polar_cmp(const pt &a, const pt &b) {
   return make_tuple(half(a), 0) < make_tuple(half(b), cross(a,b));
 }
-
-struct line {
-  pt v; T c;
-  line(pt v, T c) : v(v), c(c) {}
-  line(T a, T b, T c) : v({b,-a}), c(c) {}
-  line(pt p, pt q) : v(q-p), c(cross(v,p)) {}
-  T side(pt p) { return cross(v,p)-c; }
-  lf dist(pt p) { return abs(side(p)) / abs(v); }
-  lf sq_dist(pt p) { return side(p)*side(p) / (lf)norm(v); }
-  line perp_through(pt p) { return {p, p + rot90ccw(v)}; }
-  bool cmp_proj(pt p, pt q) { return dot(v,p) < dot(v,q); }
-  line translate(pt t) { return {v, c + cross(v,t)}; }
-  line shift_left(double d) { return {v, c + d*abs(v)}; }
-  pt proj(pt p) { return p - rot90ccw(v)*side(p)/norm(v); }
-  pt refl(pt p) { return p - rot90ccw(v)*2*side(p)/norm(v); }
-};
-
-bool inter_ll(line l1, line l2, pt &out) {
-  T d = cross(l1.v, l2.v);
-  if (d == 0) return false;
-  out = (l2.v*l1.c - l1.v*l2.c) / d;
-  return true;
-}
-line bisector(line l1, line l2, bool interior) {
-  assert(cross(l1.v, l2.v) != 0); /// l1 and l2 cannot be parallel!
-  lf sign = interior ? 1 : -1;
-  return {l2.v/abs(l2.v) + l1.v/abs(l1.v) * sign,
-          l2.c/abs(l2.v) + l1.c/abs(l1.v) * sign};
-}
-
 bool in_disk(pt a, pt b, pt p) {
   return dot(a-p, b-p) <= 0;
 }
@@ -194,135 +131,6 @@ enum {IN, OUT, ON};
 struct polygon {
   vector<pt> p;
   polygon(int n) : p(n) {}
-  int top = -1, bottom = -1;
-  void delete_repetead() {
-    vector<pt> aux;
-    sort(p.begin(), p.end());
-    for(pt &i : p)
-      if(aux.empty() || aux.back() != i)
-        aux.push_back(i);
-    p.swap(aux);
-  }
-  bool is_convex() {
-    bool pos = 0, neg = 0;
-    for (int i = 0, n = p.size(); i < n; i++) {
-      int o = orient(p[i], p[(i+1)%n], p[(i+2)%n]);
-      if (o > 0) pos = 1;
-      if (o < 0) neg = 1;
-    }
-    return !(pos && neg);
-  }
-  lf area(bool s = false) {
-    lf ans = 0;
-    for (int i = 0, n = p.size(); i < n; i++)
-      ans += cross(p[i], p[(i+1)%n]);
-    ans /= 2;
-    return s ? ans : abs(ans);
-  }
-  lf perimeter() {
-    lf per = 0;
-    for(int i = 0, n = p.size(); i < n; i++)
-      per += abs(p[i] - p[(i+1)%n]);
-    return per;
-  }
-  bool above(pt a, pt p) { return p.y >= a.y; }
-  bool crosses_ray(pt a, pt p, pt q) {
-    return (above(a,q)-above(a,p))*orient(a,p,q) > 0;
-  }
-  int in_polygon(pt a) {
-    int crosses = 0;
-    for(int i = 0, n = p.size(); i < n; i++) {
-      if(on_segment(p[i], p[(i+1)%n], a)) return ON;
-      crosses += crosses_ray(a, p[i], p[(i+1)%n]);
-    }
-    return (crosses&1 ? IN : OUT);
-  }
-  void normalize() { /// polygon is CCW
-    bottom = min_element(p.begin(), p.end()) - p.begin();
-    vector<pt> tmp(p.begin()+bottom, p.end());
-    tmp.insert(tmp.end(), p.begin(), p.begin()+bottom);
-    p.swap(tmp);
-    bottom = 0;
-    top = max_element(p.begin(), p.end()) - p.begin();
-  }
-  int in_convex(pt a) {
-    assert(bottom == 0 && top != -1);
-    if(a < p[0] || a > p[top]) return OUT;
-    T orientation = orient(p[0], p[top], a);
-    if(orientation == 0) {
-      if(a == p[0] || a == p[top]) return ON;
-      return top == 1 || top + 1 == p.size() ? ON : IN;
-    } else if (orientation < 0) {
-      auto it = lower_bound(p.begin()+1, p.begin()+top, a);
-      T d = orient(*prev(it), a, *it);
-      return d < 0 ? IN : (d > 0 ? OUT: ON);
-    }
-    else {
-      auto it = upper_bound(p.rbegin(), p.rend()-top-1, a);
-      T d = orient(*it, a, it == p.rbegin() ? p[0] : *prev(it));
-      return d < 0 ? IN : (d > 0 ? OUT: ON);
-    }
-  }
-  polygon cut(pt a, pt b) {
-    line l(a, b);
-    polygon new_polygon(0);
-    for(int i = 0, n = p.size(); i < n; ++i) {
-      pt c = p[i], d = p[(i+1)%n];
-      lf abc = cross(b-a, c-a), abd = cross(b-a, d-a);
-      if(abc >= 0) new_polygon.p.push_back(c);
-      if(abc*abd < 0) {
-        pt out; inter_ll(l, line(c, d), out);
-        new_polygon.p.push_back(out);
-      }
-    }
-    return new_polygon;
-  }
-  void convex_hull() {
-    sort(p.begin(), p.end());
-    vector<pt> ch;
-    ch.reserve(p.size()+1);
-    for(int it = 0; it < 2; it++) {
-      int start = ch.size();
-      for(auto &a : p) {
-        /// if colineal are needed, use < and remove repeated points
-        while(ch.size() >= start+2 && orient(ch[ch.size()-2], ch.back(), a) <= 0)
-          ch.pop_back();
-        ch.push_back(a);
-      }
-      ch.pop_back();
-      reverse(p.begin(), p.end());
-    }
-    if(ch.size() == 2 && ch[0] == ch[1]) ch.pop_back();
-    /// be careful with CH of size < 3
-    p.swap(ch);
-  }
-  vector<pii> antipodal() {
-    vector<pii> ans;
-    int n = p.size();
-    if(n == 2) ans.push_back({0, 1});
-    if(n < 3) return ans;
-    auto nxt = [&](int x) { return (x+1 == n ? 0 : x+1); };
-    auto area2 = [&](pt a, pt b, pt c) { return cross(b-a, c-a); };
-    int b0 = 0;
-    while(abs(area2(p[n - 1], p[0], p[nxt(b0)])) >
-          abs(area2(p[n - 1], p[0], p[b0])))
-      ++b0;
-    for(int b = b0, a = 0; b != 0 && a <= b0; ++a) {
-      ans.push_back({a, b});
-      while (abs(area2(p[a], p[nxt(a)], p[nxt(b)])) >
-             abs(area2(p[a], p[nxt(a)], p[b]))) {
-        b = nxt(b);
-        if(a != b0 || b != 0) ans.push_back({ a, b });
-        else return ans;
-      }
-      if(abs(area2(p[a], p[nxt(a)], p[nxt(b)])) ==
-         abs(area2(p[a], p[nxt(a)], p[b]))) {
-        if(a != b0 || b != n-1) ans.push_back({ a, nxt(b) });
-        else ans.push_back({ nxt(a), b });
-      }
-    }
-    return ans;
-  }
   pt centroid() {
     pt c{0, 0};
     lf scale = 6. * area(true);
@@ -343,37 +151,6 @@ struct polygon {
   pt& operator[] (int i){ return p[i]; }
 };
 
-struct circle {
-  pt c; T r;
-};
-
-circle center(pt a, pt b, pt c) {
-  b = b-a, c = c-a;
-  assert(cross(b,c) != 0); /// no circumcircle if A,B,C aligned
-  pt cen = a + rot90ccw(b*norm(c) - c*norm(b))/cross(b,c)/2;
-  return {cen, abs(a-cen)};
-}
-int inter_cl(circle c, line l, pair<pt, pt> &out) {
-  lf h2 = c.r*c.r - l.sq_dist(c.c);
-  if(h2 >= 0) {
-    pt p = l.proj(c.c);
-    pt h = l.v*sqrt(h2)/abs(l.v);
-    out = {p-h, p+h};
-  }
-  return 1 + sign(h2);
-}
-int inter_cc(circle c1, circle c2, pair<pt, pt> &out) {
-  pt d=c2.c-c1.c; double d2=norm(d);
-  if(d2 == 0) { assert(c1.r != c2.r); return 0; } // concentric circles
-  double pd = (d2 + c1.r*c1.r - c2.r*c2.r)/2; // = |O_1P| * d
-  double h2 = c1.r*c1.r - pd*pd/d2; // = h�2
-  if(h2 >= 0) {
-    pt p = c1.c + d*pd/d2, h = rot90ccw(d)*sqrt(h2/d2);
-    out = {p-h, p+h};
-  }
-  return 1 + sign(h2);
-}
-
 int tangents(circle c1, circle c2, bool inner, vector<pair<pt,pt>> &out) {
   if(inner) c2.r = -c2.r;
   pt d = c2.c-c1.c;
@@ -385,7 +162,6 @@ int tangents(circle c1, circle c2, bool inner, vector<pair<pt,pt>> &out) {
   }
   return 1 + (h2 > 0);
 }
-
 int tangent_through_pt(pt p, circle c, pair<pt, pt> &out) {
   double d = abs(p - c.c);
 	if(d < c.r) return 0;
