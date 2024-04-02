cc9ecb9926b18ba22a93f5c47c93eef0e33efe82
joacarrilloco <30640717+joacarrilloco@users.noreply.github.com>
2024-04-02 09:31:17 -0700

    Update Fast Fourier transform module.cpp
    Modified default módulo to 998'244'353

--- Math/Fast Fourier transform module.cpp
+++ Math/Fast Fourier transform module.cpp
@@ -1,11 +1,3 @@
-/// Complexity: O(|N|*log(|N|))
-/// Tested: https://tinyurl.com/yagvw3on
-const int mod = 7340033; /// mod = c*2^k+1
-/// find g = primitive root of mod.
-const int root = 2187; /// (g^c)%mod
-const int root_1 = 4665133; /// inverse of root
-const int root_pw = 1 << 19; /// 2^k
-
 pii find_c_k(int mod) {
   pii ans;
   for(int k = 1; (1<<k) < mod; k++) {
@@ -32,40 +24,61 @@ int find_primitive_root(int mod) {
   assert(false);
 }
 
-void fft(vector<int> &a, bool inv = 0) {
-  int n = a.size();
-  for(int i = 1, j = 0; i < n; i++) {
-    int c = n >> 1;
-    for (; j >= c; c >>= 1) j -= c;
-    j += c;
-    if(i < j) swap(a[i], a[j]);
-  }
-  for (int len = 2; len <= n; len <<= 1) {
-    int wlen = inv ? root_1 : root;
-    for(int i = len; i < root_pw; i <<= 1) wlen = (1 LL * wlen * wlen) % mod;
-    for(int i = 0; i < n; i += len) {
-      int w = 1;
-      for(int j = 0; j < (len >> 1); j++) {
-        int u = a[i + j], v = (a[i + j + (len >> 1)] * 1 LL * w) % mod;
-        a[i + j] = u + v < mod ? u + v : u + v - mod;
-        a[i + j + (len >> 1)] = u - v >= 0 ? u - v : u - v + mod;
-        w = (w * 1 LL * wlen) % mod;
+struct FFT
+{
+  int mod, root, root_1, root_pw;
+
+  void fft(vector<int> & a, bool invert) {
+      int n = a.size();
+      for (int i = 1, j = 0; i < n; ++i) {
+          int bit = n >> 1;
+          for (; j & bit; bit >>= 1)
+              j ^= bit;
+          j ^= bit;
+          if (i < j) swap(a[i], a[j]);
       }
-    }
+
+      for (int len = 2; len <= n; len <<= 1) {
+          int wlen = invert ? root_1 : root;
+          for (int i = len; i < root_pw; i <<= 1)
+              wlen = 1LL * wlen * wlen % mod;
+
+          for (int i = 0; i < n; i += len) {
+              int w = 1;
+              for (int j = 0; j < len / 2; ++j) {
+                  int u = a[i+j], v = 1LL * a[i+j+len/2] * w % mod;
+                  a[i+j] = u + v < mod ? u + v : u + v - mod;
+                  a[i+j+len/2] = u - v >= 0 ? u - v : u - v + mod;
+                  w = 1LL * w * wlen % mod;
+              }
+          }
+      }
+
+      if (invert) {
+          int n_1 = pw(n, mod-2, mod);
+          for (int & x : a)
+              x = 1LL * x * n_1 % mod;
+      }
   }
-  if (inv) {
-    int nrev = pow(n);
-    for(int i = 0; i < n; i++) a[i] = (a[i] * 1 LL * nrev) % mod;
+
+  vector<int> multiply(vector<int> const& a, vector<int> const& b) {
+      vector<int> fa(a.begin(), a.end()), fb(b.begin(), b.end());
+      int n = 1;
+      while (n < a.size() + b.size()) 
+          n <<= 1;
+
+      fa.resize(n);
+      fb.resize(n);
+
+      fft(fa, false);
+      fft(fb, false);
+
+      for (int i = 0; i < n; i++)
+          fa[i] = 1LL * fa[i] * fb[i] % mod;    
+      fft(fa, true);
+      
+      return fa;
   }
-}
-vector<int> mul(const vector <int> a, const vector <int> b) {
-  vector<int> fa(a.begin(), a.end()), fb(b.begin(), b.end());
-  int n = 1;
-  while (n < max(a.size(), b.size())) n <<= 1;
-  n <<= 1;
-  fa.resize(n); fb.resize(n);
-  fft(fa); fft(fb);
-  for (int i = 0; i < n; i++) fa[i] = (1ll * fa[i] * fb[i]) % mod;
-  fft(fa, 1);
-  return fa;
-}
+};
+
+FFT A = { 998244353, 15311432, 469870224, 1<<23 };
